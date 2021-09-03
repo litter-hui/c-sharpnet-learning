a851aaa8ffe9b3cecb8a36495cd370ca2ef68023
Litter_hui <1973999197@qq.com>
2021-09-03 11:26:06 +0800

    This is s text

--- HelloWorld/Demo/Demo/Program.cs
+++ HelloWorld/Demo/Demo/Program.cs
@@ -10,7 +10,7 @@ namespace Demo
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World");
+            Console.WriteLine("这是我的第一个程序");
         }
     }
 }
